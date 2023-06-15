81c2fc7b9d8bcb28bd91b4d15ab41f601d92c99e
Pece <Pece@DESKTOP-O5HTMJP>
2023-06-15 10:39:36 +0200

    Add project files.

---
+++ PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Data;
+using SEDC.PizzaApp.Web.Mapper;
+using SEDC.PizzaApp.Web.Models.Domain;
+using SEDC.PizzaApp.Web.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var pizzas = PizzaAppDb.Pizzas.Select(x => new PizzaViewModel
+            //{
+            //    Id = x.Id,
+            //    Name = x.Name,
+            //    Price = x.Price,
+            //    IsOnPromotion = x.IsOnPromotion
+
+            //});
+            ViewBag.User = PizzaAppDb.Users.First().ToViewModel();
+            ViewData["User"] = PizzaAppDb.Users.First().ToViewModel();
+
+            List<PizzaViewModel> viewModels = PizzaAppDb.Pizzas.Select(x => x.ToViewModel()).ToList();
+
+            return View(viewModels);
+
+            // return View(new PizzaOverviewModel) se sho ni treba vo eden model
+        }
+
+        [HttpGet("order/{id}/pizza/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("order/{id}/pizza/create")]
+        public IActionResult Create(int id, CreatePizzaViewModel model)
+        {
+            var order = PizzaAppDb.Orders.FirstOrDefault(x => x.Id == id);
+
+            if (order == null)
+            {
+                return View("NotFoundView");
+            }
+
+            var pizza = new Pizza(order, model.Name, model.Price)
+            {
+                Id = PizzaAppDb.Pizzas.Max(x => x.Id) + 1,
+                IsOnPromotion = model.IsOnPromotion
+            };
+            order.Pizzas.Add(pizza);
+
+            PizzaAppDb.Pizzas.Add(pizza);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PizzaApp/Data/PizzaAppDb.cs
@@ -0,0 +1,47 @@
+﻿using SEDC.PizzaApp.Web.Models.Domain;
+
+namespace SEDC.PizzaApp.Web.Data
+{
+    public static class PizzaAppDb
+    {
+        public static ICollection<Pizza> Pizzas { get; set; } = new List<Pizza>
+        {
+            new Pizza
+            {
+                Id = 1,
+                Name = "Margarita",
+                Price = 200,
+                IsOnPromotion = false
+            },
+            new Pizza
+            {
+                Id = 2,
+                Name = "Special",
+                Price = 350,
+                IsOnPromotion = false
+            }
+        };
+
+        public static ICollection<Order> Orders { get; set; } = new List<Order>()
+        {
+            new Order
+            {
+                Id = 1,
+                PaymentMethod = Models.Enums.PaymentMethod.Card,
+                User = new User("Jovan", "Gjorgjiev", "123/123/123")
+                {
+                    Id = 1,
+                }
+            }
+        };
+
+        public static ICollection<User> Users { get; set; } = new List<User>
+        {
+            new User("Jovan", "Gjorgjiev", "123/123/123")
+            {
+                    Id = 1,
+
+            }
+        };
+    }
+}
